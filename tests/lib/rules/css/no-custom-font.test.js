373458f95e91a2c275bc240de48dd5cd79ad230c
Utarwyn <maxime.malgorn@laposte.net>
2022-06-03 13:13:40 +0200

    Fix no-custom-font test

--- tests/lib/rules/css/no-custom-font.test.js
+++ tests/lib/rules/css/no-custom-font.test.js
@@ -1,51 +1,39 @@
 const { createTestRule } = require('./rule-tester');
 
 const { ruleFilePath, testRule } = createTestRule('no-custom-font');
-const { ruleName } = require(ruleFilePath);
+const { messages, ruleName } = require(ruleFilePath);
 
 testRule({
   ruleName,
+  config: [true],
   accept: [
     {
       code: 'h4 { font-family: sans-serif }',
       description: 'use sans-serif',
     },
     {
-      code: 'h4 { font-family: Arial, sans-serif }',
-      description: 'use Arial and sans-serif',
+      code: 'h4 { font-family: monospace, sans-serif }',
+      description: 'use monospace or sans-serif',
     },
   ],
-  // TODO fix this
-  // reject: [
-  //   {
-  //     code: 'h4 { font-family: Code Pro; }',
-  //     description: 'use Code Pro custom font family',
-  //     message: messages.expected(),
-  //     line: 1,
-  //     column: 6,
-  //     endLine: 1,
-  //     endColumn: 8,
-  //   },
-  //   {
-  //     code: '.MyClass,\n.MyOtherClass {}',
-  //     fixed: '.my-class,\n.my-other-class {}',
-  //     description: 'two pascal class selectors in a selector list',
-  //     warnings: [
-  //       {
-  //         message: messages.expected(),
-  //         line: 1,
-  //         column: 1,
-  //         endLine: 1,
-  //         endColumn: 8,
-  //       },
-  //       {
-  //         message: messages.expected(),
-  //         line: 2,
-  //         column: 1,
-  //         endLine: 2,
-  //         endColumn: 13,
-  //       },
-  //     ],
-  //   },
-  // ],
+  reject: [
+    {
+      code: 'h4 { font-family: Code Pro; }',
+      description: 'use Code Pro custom font family',
+      message: messages.expected('Code Pro'),
+      line: 1,
+      column: 19,
+      endLine: 1,
+      endColumn: 27,
+    },
+    {
+      code: 'h4 { font-family: Arial, sans-serif; }',
+      description: 'use Arial custom font family and sans-serif',
+      message: messages.expected('Arial'),
+      line: 1,
+      column: 19,
+      endLine: 1,
+      endColumn: 24,
+    },
+  ],
 });
