dccfe626a76437cc75dd4c106e423e68f00140c4
Coldknife2 <remi.sylux@gmail.com>
2022-11-22 16:30:49 +0100

    feat(js): adding js rule s39 'batch multiple css changes' (#9)
    Co-authored-by: Rémi Boussu <remi.boussu@axa.fr>

---
+++ tests/lib/rules/js/multiple-css-changes.js
@@ -0,0 +1,83 @@
+/**
+ * @fileoverview Multiple CSS change
+ * @author Rémi Boussu
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../../lib/rules/js/multiple-css-changes');
+const RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run('multiple-css-changes', rule, {
+  valid: [
+    {
+      code: 'element.style.height = "800px";'
+    },
+    {
+      code: 
+      `element.style.height = "800px";
+      element2.style.width = "800px";`
+    },
+    {
+      code: 
+      `element.style.height = "800px";
+      function a() { element.style.width = "800px"; }
+      `
+    }
+  ],
+
+  invalid: [
+    {
+      code: 
+        `function a(element){
+          element.style.height = "800px";
+          element.style.width = "800px";
+        }`,
+      errors: [
+        {
+          message: "More than two style assignation",
+          type: 'AssignmentExpression',
+        },
+      ],
+    },
+    {
+      code: 
+      `element.style.height = "800px";
+      element.style.width = "800px";`,
+      errors: [
+        {
+          message: "More than two style assignation",
+          type: 'AssignmentExpression',
+        },
+      ],
+    },
+    {
+      code: 
+      `
+      function changeStyle()
+      {
+        var anyScopedVar;
+        element.style.any = "800px";
+        anotherChildElement.style.any = "800px";
+        anyGlobalVar.assignation = "any";
+        anyScopedVar = anyGlobalVar.assignation;
+        element.style.anyOther = "800px";
+      }
+      `,
+      errors: [
+        {
+          message: "More than two style assignation",
+          type: 'AssignmentExpression',
+        },
+      ],
+    }
+  ],
+});
