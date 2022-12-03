78f99d71c50a2ee3fcd69f74a09a8a1599914540
Meyrueis de Jurquet <meyrueis.de-jurquet@epitech.eu>
2022-12-03 05:43:00 +0100

    udp: style

--- tests/lib/rules/js/eco-font-family-use.js
+++ tests/lib/rules/js/eco-font-family-use.js
@@ -2,54 +2,52 @@
  * @fileoverview prevent use of custom font for eco coding
  * @author alexandre nathan
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../../lib/rules/js/eco-font-family-use");
-const RuleTester = require("eslint").RuleTester;
+const rule = require('../../../../lib/rules/js/eco-font-family-use');
+const RuleTester = require('eslint').RuleTester;
 
 RuleTester.setDefaultConfig({
   parserOptions: {
     ecmaVersion: 6,
     ecmaFeatures: {
-      "jsx": true
+      jsx: true,
     },
   },
 });
 
 const TemplateElement = {
-  message: "Your font-family is not in the standard fonts family : Impact",
+  message: 'Your font-family is not in the standard fonts family : Impact',
   //type: "TemplateElement"
 };
 
 const Identifier = {
-  message: "Your font-family is not in the standard fonts family : Azerty",
+  message: 'Your font-family is not in the standard fonts family : Azerty',
   //type: "Identifier"
 };
-
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
-ruleTester.run("eco-font-family-use", rule, {
+ruleTester.run('eco-font-family-use', rule, {
   valid: [
-      "const style={fontFamily: 'serif'}",
-      "const Button = styled.input`font-family: 'serif';`"
+    "const style={fontFamily: 'serif'}",
+    "const Button = styled.input`font-family: 'serif';`",
   ],
   invalid: [
     {
       code: "const style={fontFamily: 'Azerty'}",
-      errors: [Identifier]
+      errors: [Identifier],
     },
     {
       code: "const Button = styled.input`font-family: 'Impact';`",
-      errors: [TemplateElement]
+      errors: [TemplateElement],
     },
   ],
 });
-
